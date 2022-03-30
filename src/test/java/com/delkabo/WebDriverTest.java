79e859f5dc5df7faa778902a79d9f4db6645680d
delkabo <delkabo@yandex.ru>
2022-03-31 00:48:52 +0300

    add WebDriverTestDemo for example how add remote to demopage

--- src/test/java/com/delkabo/WebDriverTest.java
+++ src/test/java/com/delkabo/WebDriverTest.java
@@ -1,10 +1,12 @@
 package com.delkabo;
 
 import com.delkabo.config.WebDriverProvider;
+import org.assertj.core.configuration.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,7 +22,6 @@ public class WebDriverTest {
     @Test
     public void testGitHubTitle() {
         assertEquals(driver.getTitle(), "GitHub: Where the world builds software · GitHub");
-
     }
 
     @AfterEach
---
+++ src/test/java/com/delkabo/WebDriverTestDemo.java
@@ -0,0 +1,35 @@
+package com.delkabo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.net.MalformedURLException;
+
+public class WebDriverTestDemo {
+
+//    private WebDriver driver;
+//
+//    FirefoxOptions firefoxOptions = new FirefoxOptions();
+//
+//    private WebDriver driver2;
+//
+//    @BeforeEach
+//    public void setUp() throws MalformedURLException {
+//        DriverSettings.configure();
+//        //driver = new WebDriverProvider().get();
+//        //driver2 = new RemoteWebDriver(new URL("https://user1:1234@selenoid.autotests.cloud/wd/hub/"), firefoxOptions);
+//    }
+//
+//    @Test
+//    public void getConfigTest() {
+////        open("https://opennet.ru");
+//        open("https://yandex.ru/internet/");
+////        driver.getTitle();
+//        //parameter-wrapper__parameter-header
+////        driver2.get("https://yandex.ru/internet/");
+////        System.out.println(driver2.getPageSource());
+//        System.out.println(source());
+
+}
